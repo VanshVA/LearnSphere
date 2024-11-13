19d973210aab3d95eb97f8cd05cf0cde7623a174
Anuj-prajapati-SDE <anujprajapa3@gmail.com>
2024-11-13 20:22:47 +0530

    Initial Server Code

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,10 @@
-console.log('hello');
+import express from "express"
+const app = express();
+const PORT = 8080;
+app.get('/', (req, res) => {
+    res.send(`Server Started`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening at http://localhost:${PORT}`);
+});
