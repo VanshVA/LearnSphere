0fbf0252cb244fe29a2e3a81e2d40296bfabbfaa
VanshVA <honourpc29@gmail.com>
2024-11-16 15:39:42 +0530

    Add Authentication System [Frontend] and loading Component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Homepage from './pages/Homepage/Homepage';
+import Authentication from './Authentication/Authentication';
+import Logo from './components/Logo/Logo';
 const App = () => {
 
   return (
     <>
-      <Router>
+      {/* <Router>
         <Routes>
           <Route element={<Homepage/>} path='/'/>
         </Routes>
-      </Router>    
+      </Router>     */}
+     <Logo></Logo>
     </>
   )
 }
