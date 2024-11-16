7b2d8dde09aec078089dbdb38ebd89ef94073434
Shaily Tyagi <shailytyagi013@gmail.com>
2024-11-16 12:31:19 +0530

    modified

--- Frontend/src/pages/Homepage/CoursesSection/CoursesSection.jsx
+++ Frontend/src/pages/Homepage/CoursesSection/CoursesSection.jsx
@@ -3,7 +3,32 @@ import './CoursesSection.css'
 
 const CoursesSection = () => {
   return (
-    <div className='courses-section-container'>CoursesSection by Shaily</div>
+    <div className='main'>
+              { /* OUR TRANDING COURSES */}
+      <section>
+        <h1>Our Trending Courses</h1>
+        <div className='container'>
+          <div className='card'>
+            <div className='Cardimage'>
+              <img src=''></img>
+            </div>
+            <h2>How to write a powerful speech</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.  Culpa officia sunt, excepturi dolores itaque cumque.</p>
+            <div>
+              <button>Enroll the course</button>
+              <h2>Rupees</h2>
+            </div>
+
+          </div>
+
+        </div>
+
+      </section>
+
+      <section>
+
+      </section>
+    </div>
   )
 }
 
