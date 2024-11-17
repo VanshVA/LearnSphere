76fd7e7585b6ee1fd562bfe6f199002678c66002
Anuj-prajapati-SDE <anujprajapa3@gmail.com>
2024-11-17 16:01:47 +0530

    Added FAQ section in home page

---
+++ Frontend/src/constants/FAQ-TEXT/Faq_text.js
@@ -0,0 +1,27 @@
+let FAQ_DATA = [
+{   faq_count:1,
+    faq_section_title: "Two Very Different Australias?",
+    faq_section_description:"A Google Docs scam that appears to be widespread began landing in users’ inboxes on Wednesday in what seemed to be a sophisticated phishing."
+},
+{   faq_count:2,
+    faq_section_title: "What Does Darwin Mean to You?",
+    faq_section_description:"A Google Docs scam that appears to be widespread began landing in users’ inboxes on Wednesday in what seemed to be a sophisticated phishing."
+},
+{   faq_count:3,
+    faq_section_title: "Where Totem Poles Are a Living Art ?",
+    faq_section_description:"A Google Docs scam that appears to be widespread began landing in users’ inboxes on Wednesday in what seemed to be a sophisticated phishing."
+},
+{   faq_count:4,
+    faq_section_title: "Two Very Different Australias?",
+    faq_section_description:"A Google Docs scam that appears to be widespread began landing in users’ inboxes on Wednesday in what seemed to be a sophisticated phishing."
+},
+{   faq_count:5,
+    faq_section_title: "What Does Darwin Mean to You?",
+    faq_section_description:"A Google Docs scam that appears to be widespread began landing in users’ inboxes on Wednesday in what seemed to be a sophisticated phishing."
+},
+{   faq_count:6,
+    faq_section_title: "Where Totem Poles Are a Living Art ?",
+    faq_section_description:"A Google Docs scam that appears to be widespread began landing in users’ inboxes on Wednesday in what seemed to be a sophisticated phishing."
+},
+]
+export default FAQ_DATA;
