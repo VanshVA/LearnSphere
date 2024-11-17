4f11c04fabd6321e1422c401af12fdaa431ff141
Dhruv Kashyap <dhruv.kashyap1111@gmail.com>
2024-11-17 14:29:54 +0530

    created sidebar

---
+++ Frontend/src/utils/Sidebar/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Sidebar.css'
+
+const Sidebar = ({isOpen}) => {
+    
+  return (
+    <div className={`sidebar ${ isOpen? "open" : " "}`}>
+        <a href="/">Home</a>
+        <a href="#about">About</a>
+        <a href="#course">Courses</a>
+        <a href="#contact">Contact</a>
+
+    </div>
+  )
+}
+
+export default Sidebar
